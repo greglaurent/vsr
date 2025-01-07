fead6daeaeeae844ab6516fe596dd80cec5700c3
Gregory Laurent <gregory.m.laurent@gmail.com>
2025-01-06 16:16:47 -0800

    consider view_change

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::collections::VecDeque;
-
+#[allow(dead_code, unused)]
 use axum::{response::Html, routing::get, Router};
+use viewstamp::Replica;
 
 pub mod viewstamp {
     use std::{
         collections::{HashMap, VecDeque},
-        net::IpAddr,
+        net::SocketAddr,
         sync::atomic::{AtomicU64, AtomicUsize},
     };
 
     use scru128::Scru128Id;
 
     pub type Id = Scru128Id;
-    pub type Configuration = VecDeque<IpAddr>;
+    pub type Configuration = VecDeque<SocketAddr>;
     pub type RequestId = AtomicU64;
     pub type Log = VecDeque<Request>;
 
@@ -50,10 +50,11 @@ pub mod viewstamp {
         pub curr_op: RequestId,
         pub last_op: RequestId,
         pub client_table: HashMap<Id, Vec<Record>>,
+        status: ReplicaStatus,
     }
 
     impl Replica {
-        pub fn new(ip: IpAddr) -> Self {
+        pub fn new(ip: SocketAddr) -> Self {
             let mut repl = Self {
                 id: new_id(),
                 config: VecDeque::new(),
@@ -62,12 +63,23 @@ pub mod viewstamp {
                 curr_op: AtomicU64::default(),
                 last_op: AtomicU64::default(),
                 client_table: HashMap::default(),
+                status: ReplicaStatus::ViewChange,
             };
 
             repl.config.push_back(ip);
 
+            assert!(repl.config.len() == 1);
+            assert!(repl.config.get(0) == Some(&ip));
+
+            assert!(repl.repl_num.load(std::sync::atomic::Ordering::SeqCst) == 0);
+            assert!(repl.curr_op.load(std::sync::atomic::Ordering::SeqCst) == 0);
+            assert!(repl.last_op.load(std::sync::atomic::Ordering::SeqCst) == 0);
+            assert!(repl.client_table.len() == 0);
+
             repl
         }
+
+        fn view_change(&mut self) {}
     }
 }
 
@@ -75,7 +87,10 @@ pub mod viewstamp {
 async fn main() {
     let app = Router::new().route("/", get(handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:0").await.unwrap();
+    let ip = listener.local_addr().unwrap();
+
+    let _ = Replica::new(ip);
 
     axum::serve(listener, app).await.unwrap();
 }
